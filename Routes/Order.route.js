371ca670f69802ddd64c516f8bc8c61f9a7256ad
RathaMnusSmos <dalyloy13@gmail.com>
2024-03-06 00:12:28 +0700

    oder module

--- Routes/Order.route.js
+++ Routes/Order.route.js
@@ -1,8 +1,13 @@
 const orderController = require('../controller/Order.controller')
-const {userGuard} = require('../middleware/auth.middleware')
+const {userGuard, adminGuard} = require('../middleware/auth.middleware')
 
 const order = (app) =>{
-    app.post('/api/order/initiate',userGuard, orderController.checkOutCart)
+    app.get('/api/order/initiate',userGuard, orderController.checkOutCart)
+    app.post('/api/order/ordering', userGuard, orderController.placeOrder)
+    app.get('/api/order/get-history_order', userGuard, orderController.getOrderHistoryByUserId)
+    app.get('/api/order/getAllOrder', userGuard, adminGuard, orderController.getAllOrdersForAdmin)
+
+
 }
 
 module.exports = order
